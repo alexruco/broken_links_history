32badd67432f4548c6fd624e90596dfcbeb2eeb8
Alex Ruco <alex@ruco.pt>
2024-07-28 23:09:52 +0100

    Fix setup.py name and URL fields

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
-    name="waybackmachine_site_pages ",
+    name="waybackmachine_site_pages",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -13,7 +11,7 @@ setup(
     author="Alex Ruco",
     author_email="alex@ruco.pt",
     description="A package to check for links on Wayback Machine",
-    url="https://github.com/yourusername/waybackmachine_site_pages ",
+    url="https://github.com/yourusername/waybackmachine_site_pages",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
