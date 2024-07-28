749a20cb0d080ab1713e1ae38af59ce097413011
Alex Ruco <alex@ruco.pt>
2024-07-28 11:32:07 +0100

    implemented iterative fetching

--- broken_links_history.py
+++ broken_links_history.py
@@ -1,28 +1,40 @@
-#     broken_links_history.py
+# broken_links_history.py
 
 from get_history import get_wayback_urls, filter_urls
 from availability_checker import check_availability
+import time
 
-def get_wayback_uls(domain):
-    urls_df = get_wayback_urls(domain)
-    filtered_urls_df = filter_urls(urls_df)
+def get_wayback_uls(domain, iterations=10):
+    broken_links_set = set()
 
-    # Display filtered URLs
-    if not filtered_urls_df.empty:
-        urls = filtered_urls_df["original"].tolist()
-        live_urls, broken_urls = check_availability(urls)
+    for i in range(iterations):
+        urls_df = get_wayback_urls(domain)
+        filtered_urls_df = filter_urls(urls_df)
 
-        print("Live URLs:")
-        for url in live_urls:
-            print(url)
+        if not filtered_urls_df.empty:
+            urls = filtered_urls_df["original"].tolist()
+            live_urls, broken_urls = check_availability(urls)
+            print(f"broken_urls found:{broken_urls}")
+            # Add broken URLs to the set
+            broken_links_set.update(broken_urls)
 
+            print(f"Iteration {i + 1}/{iterations} completed.")
+            time.sleep(1)  # Adding a delay to avoid overwhelming the server
+        else:
+            print("No contentful URLs found.")
+            break
+
+    # Display broken URLs
+    if broken_links_set:
         print("\nBroken URLs:")
-        for url in broken_urls:
+        for url in broken_links_set:
             print(url)
     else:
-        print("No contentful URLs found.")
+        print("No broken URLs found.")
+
+    return broken_links_set
 
 # Example usage
 if __name__ == "__main__":
-    domain = "marketividade.com"
+    domain = "example.com"
     get_wayback_uls(domain)
